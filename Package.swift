bf77edcd0ec2f6908ef8c5d5a4bfa6104c56b4e6
Matt McMurry <matt@seis.com>
2026-01-13 12:46:13 -0700

    Use pre-built TensorFlowLite.xcframework from CocoaPods
    All targets are now binary xcframeworks - no source compilation needed.
    This avoids iOS version conflicts with other SPM packages.
    
    Includes:
    - VouchedCore.xcframework
    - VouchedBarcode.xcframework
    - TensorFlowLiteC.xcframework
    - TensorFlowLite.xcframework (built from CocoaPods)

--- Package.swift
+++ Package.swift
@@ -8,9 +8,8 @@ let package = Package(
     name: "VouchedSPM",
     platforms: [.iOS(.v15)],
     products: [
-        // Expose binary frameworks directly - consumer imports VouchedCore/VouchedBarcode
-        .library(name: "VouchedCore", targets: ["VouchedCoreFramework", "TensorFlowLiteC"]),
-        .library(name: "VouchedBarcode", targets: ["VouchedBarcodeFramework", "VouchedCoreFramework", "TensorFlowLiteC"]),
+        .library(name: "VouchedCore", targets: ["VouchedCoreFramework", "TensorFlowLite", "TensorFlowLiteC"]),
+        .library(name: "VouchedBarcode", targets: ["VouchedBarcodeFramework", "VouchedCoreFramework", "TensorFlowLite", "TensorFlowLiteC"]),
     ],
     targets: [
         .binaryTarget(
@@ -27,6 +26,11 @@ let package = Package(
             name: "TensorFlowLiteC",
             url: "https://github.com/Seis-Inc/VouchedSPM/releases/download/v1.9.9/TensorFlowLiteC.xcframework.zip",
             checksum: "aaf799a6bd82500a6d4f5445a886ff0024a43c6a1fbbbdaa048470a4b9d90432"
+        ),
+        .binaryTarget(
+            name: "TensorFlowLite",
+            url: "https://github.com/Seis-Inc/VouchedSPM/releases/download/v1.9.9/TensorFlowLite.xcframework.zip",
+            checksum: "02bc182e39462456a5a310b32f7698eef33f25047c3a9c032bbf606eb2445d45"
         ),
     ]
 )
